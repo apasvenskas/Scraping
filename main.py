04e994278f476145181b4a5fec04fca3a8e7e8a8
Audrius <apasvenskas@outlook.com>
2025-05-02 19:00:54 -0700

    app working as intended

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import smtplib, ssl
 import os
 from dotenv import load_dotenv
+import sqlite3
 
 db_file = "data.db"
 
@@ -14,6 +15,8 @@ load_dotenv()
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
 
 def scrape(url):
     """ Scrape the pages source from webpage URL. """
@@ -53,22 +56,31 @@ def send_email(message):
         print("Email sent successfully!")
 
 def store(extraction):
-    with open("data.txt", "a") as file:
-        file.write(extraction + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(f"All rows in events table: {rows}")
+    return rows
 
 if __name__ == "__main__":
     while True:
-
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted) 
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extrated not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event was found!")
         time.sleep(2) # keeps the script running non stop, evry 2 seconds
